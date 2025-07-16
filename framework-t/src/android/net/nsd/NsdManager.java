ce1d010c49ae1ba94711a59d7c3842c15f16f700
Fred Underwood <underwoodfred@proton.me>
2025-07-16 20:55:08 +1000

    prevent NsdService#connect for lockdown VPN clients
    Squashed with 41a3812d85334d7c9e99645f319c30edb1e03199

--- framework-t/src/android/net/nsd/NsdManager.java
+++ framework-t/src/android/net/nsd/NsdManager.java
@@ -1183,8 +1183,13 @@ public final class NsdManager {
             mService = createStubbedINsdServiceConnector();
         } else {
             try {
-                mService = service.connect(new NsdCallbackImpl(mHandler), CompatChanges.isChangeEnabled(
+                INsdServiceConnector connector = service.connect(new NsdCallbackImpl(mHandler), CompatChanges.isChangeEnabled(
                         ENABLE_PLATFORM_MDNS_BACKEND), context.getPackageName());
+                if (connector == null) {
+                    mService = createStubbedINsdServiceConnector();
+                } else {
+                    mService = connector;
+                }
             } catch (RemoteException e) {
                 throw new RuntimeException("Failed to connect to NsdService");
             }
