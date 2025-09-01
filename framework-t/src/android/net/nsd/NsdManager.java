80cf200e43069b91a4f4908e64ed04274907e000
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-09-01 19:48:20 +0300

    add stub INsdServiceConnector implementation

--- framework-t/src/android/net/nsd/NsdManager.java
+++ framework-t/src/android/net/nsd/NsdManager.java
@@ -1112,6 +1112,58 @@ public final class NsdManager {
         }
     }
 
+    private INsdServiceConnector createStubbedINsdServiceConnector() {
+        return new NsdServiceConnectorStub(mHandler);
+    }
+
+    static class NsdServiceConnectorStub implements INsdServiceConnector {
+        final NsdCallbackImpl callback;
+
+        private NsdServiceConnectorStub(Handler handler) {
+            callback = new NsdCallbackImpl(handler);
+        }
+
+        @Override public void registerService(int listenerKey,
+                AdvertisingRequest advertisingRequest) {
+            callback.onRegisterServiceFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void unregisterService(int listenerKey) {
+            callback.onUnregisterServiceFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void discoverServices(int listenerKey,
+                DiscoveryRequest discoveryRequest) {
+            callback.onDiscoverServicesFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void stopDiscovery(int listenerKey) {
+            callback.onStopDiscoveryFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void resolveService(int listenerKey, NsdServiceInfo serviceInfo) {
+            callback.onResolveServiceFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void startDaemon() {}
+        @Override public void stopResolution(int listenerKey) {
+            callback.onStopResolutionFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void registerServiceInfoCallback(int listenerKey,
+                NsdServiceInfo serviceInfo) {
+            callback.onServiceInfoCallbackRegistrationFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void registerServiceInfoCallbackWithRequest(int listenerKey, DiscoveryRequest discoveryRequest) {
+            callback.onServiceInfoCallbackRegistrationFailed(listenerKey, FAILURE_INTERNAL_ERROR);
+        }
+        @Override public void unregisterServiceInfoCallback(int listenerKey) {
+            callback.onServiceInfoCallbackUnregistered(listenerKey);
+        }
+        @Override public void registerOffloadEngine(String ifaceName, android.net.nsd.IOffloadEngine cb,
+                long offloadCapabilities, long offloadType) {}
+        @Override public void unregisterOffloadEngine(android.net.nsd.IOffloadEngine cb) {}
+        @Override public void injectOffloadEngineResponse(NsdServiceInfo serviceInfo, boolean isServiceLost, String ifaceName) {}
+        @Override public void checkPermissionForService(String serviceName, String serviceType, ResultReceiver resultReceiver) {
+            resultReceiver.send(SERVICE_PERMISSION_DENIED, null);
+        }
+        @Override public android.os.IBinder asBinder() { return null; }
+    }
+
     /**
      * Create a new Nsd instance. Applications use
      * {@link android.content.Context#getSystemService Context.getSystemService()} to retrieve
